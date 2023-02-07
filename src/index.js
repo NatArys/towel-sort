71ae9f924cc88f8264a5551db01740d57fc71f5b
NatArys <91931609+NatArys@users.noreply.github.com>
2023-02-07 15:18:47 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function towelSort (matrix) {
-let result=matrix.map((v,i) => (i%2)? v.reverse() : v).flat()
+let result=(matrix)? matrix.map((v,i) => (i%2)? v.reverse() : v).flat()  : [];
 return result;
 }
